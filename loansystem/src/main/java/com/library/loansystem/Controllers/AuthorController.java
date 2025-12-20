2cbb279cca8f45ce8cc1ee45af26d84ef5b26160
roqueotha04 <roqueotha04@gmail.com>
2025-12-20 13:33:52 -0300

    finished Author Controller

--- loansystem/src/main/java/com/library/loansystem/Controllers/AuthorController.java
+++ loansystem/src/main/java/com/library/loansystem/Controllers/AuthorController.java
@@ -1,4 +1,47 @@
 package com.library.loansystem.Controllers;
 
+import com.library.loansystem.Entities.Author;
+import com.library.loansystem.Services.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/authors")
 public class AuthorController {
+
+    @Autowired
+    AuthorService authorService;
+
+    @GetMapping
+    public List<Author> getAll() {
+        return authorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Author getById(@PathVariable Long id) {
+        return authorService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Author create (@RequestBody Author author){
+        return authorService.save(author);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete (@PathVariable Long id){
+        authorService.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Author update (@PathVariable Long id, @RequestBody Author author){
+        return authorService.update(id, author);
+    }
+
+
 }
