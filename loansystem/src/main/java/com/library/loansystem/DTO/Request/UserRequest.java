0b7d15b3495ba2748eb3f2879c022ad4ac539ebe
roqueotha04 <roqueotha04@gmail.com>
2026-01-12 20:23:30 -0300

    added UserRequest and Response and LoanRequest and Response

---
+++ loansystem/src/main/java/com/library/loansystem/DTO/Request/UserRequest.java
@@ -0,0 +1,4 @@
+package com.library.loansystem.DTO.Request;
+
+public record UserRequest(String gmail, String username, String password) {
+}
---
+++ loansystem/src/main/java/com/library/loansystem/DTO/Response/UserResponse.java
@@ -0,0 +1,4 @@
+package com.library.loansystem.DTO.Response;
+
+public record UserResponse(Long id, String gmail, String username, String password) {
+}
