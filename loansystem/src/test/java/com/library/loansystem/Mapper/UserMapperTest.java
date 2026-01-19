bee79706ffc207e5d644bf63a9ef27d1d649016d
roqueotha04 <roqueotha04@gmail.com>
2026-01-18 21:55:19 -0300

    added method testToResponse to UserMapperTest

--- loansystem/src/test/java/com/library/loansystem/Mapper/UserMapperTest.java
+++ loansystem/src/test/java/com/library/loansystem/Mapper/UserMapperTest.java
@@ -1,4 +1,21 @@
 package com.library.loansystem.Mapper;
 
+import com.library.loansystem.DTO.Response.UserResponse;
+import com.library.loansystem.Entities.User;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserMapperTest {
+
+    UserMapper userMapper = new UserMapper();
+
+    @Test
+    public void testToResponse(){
+        User user = new User("lionelmessi@hotmail.com", "lionel", "theGoat");
+
+        UserResponse result = userMapper.toResponse(user);
+        assertEquals(user.getId(), result.id());
+        assertEquals(user.getEmail(), result.email());
+        assertEquals(user.getUsername(), result.username());
+    }
 }
