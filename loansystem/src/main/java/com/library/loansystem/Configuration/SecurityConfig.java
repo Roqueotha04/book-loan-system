afc8f58455f43f1df178cb88404116c6e24ec495
roqueotha04 <roqueotha04@gmail.com>
2026-01-30 16:32:10 -0300

    feat: add SecurityConfig class, add securityFilterChain and authenticationManager to it

---
+++ loansystem/src/main/java/com/library/loansystem/Configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.library.loansystem.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity)throws Exception{
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
