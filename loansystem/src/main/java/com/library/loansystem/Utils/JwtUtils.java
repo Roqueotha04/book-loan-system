97a46dd72aa9464d0c7fef99817365fc63f43091
roqueotha04 <roqueotha04@gmail.com>
2026-02-02 14:29:56 -0300

    feat: add method validateToken, extractUsername, getSpecificClaim and getAllClaims to JwtUtils

--- loansystem/src/main/java/com/library/loansystem/Utils/JwtUtils.java
+++ loansystem/src/main/java/com/library/loansystem/Utils/JwtUtils.java
@@ -1,13 +1,18 @@
 package com.library.loansystem.Utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -24,7 +29,7 @@ public class JwtUtils {
     public String createToken(Authentication authentication){
         Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
 
-        String username = authentication.getPrincipal().toString();
+        String username = authentication.getName();
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
@@ -36,7 +41,31 @@ public class JwtUtils {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + 1800000))
                 .withJWTId(UUID.randomUUID().toString())
-                .withNotBefore(new Date(System.currentTimeMillis()))
                 .sign(algorithm);
+    }
+
+    public DecodedJWT validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(this.userGenerator)
+                    .build();
+
+            return verifier.verify(token);
+        }catch (JWTVerificationException exception){
+            throw new JWTVerificationException("Invalid Token, Unauthorized");
+        }
+    }
+
+    public String extractUsername(DecodedJWT decodedJWT){
+        return decodedJWT.getSubject();
+    }
+
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName){
+        return decodedJWT.getClaim(claimName);
+    }
+
+    public Map<String, Claim> getAllClaims(DecodedJWT decodedJWT){
+        return decodedJWT.getClaims();
     }
 }
